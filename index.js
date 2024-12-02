cd1e3b9866533174cfd18ad5005f7ad998f799f4
R. Thomas Talukdar <81384823+tliontube@users.noreply.github.com>
2024-12-02 19:39:50 +0530

    new update

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-import './express-app.js';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDB from "./database/connection.js";
+import config from "./config.js";
+import task from "./api/routes/tasks.js";
+import user from "./api/routes/user.js";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+task(app)
+user(app)
+
+connectDB()
+  .then(() => {
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
